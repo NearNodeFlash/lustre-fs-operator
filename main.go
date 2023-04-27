f2edcca4de8ef87182e95850ff9fb21ceba71828
Dean Roehrich <dean.roehrich@hpe.com>
2023-04-27 16:39:50 -0500

    Create v1beta1 APIs
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	lusv1alpha1 "github.com/NearNodeFlash/lustre-fs-operator/api/v1alpha1"
+	lusv1beta1 "github.com/NearNodeFlash/lustre-fs-operator/api/v1beta1"
 	"github.com/NearNodeFlash/lustre-fs-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
@@ -48,6 +49,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(lusv1alpha1.AddToScheme(scheme))
+	utilruntime.Must(lusv1beta1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
